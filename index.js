4841769734e30d6c1a7798a1fe99c643b9eaae2b
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-21 23:07:42 +0600

    night item

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//const sum = (a, b) => a + b;
+//console.log(sum(23, 44)); // Logs the sum of 23 and 44
+
+
+const sum = (a, b, c) => {
+  const result = a + b + c;
+  return result
+}
+const lts=sum(3,35,5)
+console.log(lts)
