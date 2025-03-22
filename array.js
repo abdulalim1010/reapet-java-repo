22bbb8415b59f1a05072ab1ea0945f9b756b2f6d
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-22 15:14:24 +0600

    last item

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 const products = [
   { name: "apple", brand: "applea", price: 30000, color: "black", country: "usa" },
-  { name: "vivo", brand: "chin", price: 34000, color: "blue", country: "china" },
+  { name: "vivo", brand: "applea", price: 34000, color: "blue", country: "china" },
   { name: "xoumi", brand: "tawn", price: 90000, color: "red", country: "tawayan" },
   {
     name: "sumsung", brand: "handsom", price: 50000, color: "red", country: "italy"
@@ -11,5 +11,10 @@ const products = [
   {name:"techno",brand:"heavy",price:45599,color:"green",country:"india"}
 
 ]
-const result=products.map(product=>product.brand)
+/* const result=products.filter(product=>product.brand)
 console.log(result)
+const result=products.find(product=>product.brand==="applea")
+console.log(result) */
+/* const result=products.filter(product=>product.brand==="applea")
+console.log(result) */
+products.forEach(product=>console.log(product.country))
